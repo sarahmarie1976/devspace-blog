530facd7e855b2c5a244314adab44ab0fa52d433
Sarahmarie D Hollenstein <sholle7@gmail.com>
2022-08-10 17:51:39 -0500

    :racehorse: added gray-matter and props including gray-matter

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,41 @@
+import fs from 'fs';
+import matter from 'gray-matter';
+import path from 'path';
+
 import Layout from '../components/Layout';
 
-export default function HomePage() {
+export default function HomePage({ posts }) {
+  console.log(posts);
+
   return (
     <Layout className=''>
       <h1>Hello World</h1>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join('posts'));
+
+  const posts = files.map((filename) => {
+    const slug = filename.replace('.md', '');
+
+    const markdownWithMeta = fs.readFileSync(
+      path.join('posts', filename),
+      'utf-8'
+    );
+
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
 }
