bcb28a0591f1b0f1dd71217318dc637313af21b4
Sarahmarie D Hollenstein <sholle7@gmail.com>
2022-08-13 19:48:01 -0500

    :art: cleaned up by creating a getPosts in lib folder

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,10 @@
-import fs from 'fs';
-import matter from 'gray-matter';
 import Link from 'next/link';
-import path from 'path';
 
 import Layout from '@/components/Layout';
 import Post from '@/components/Post';
-import { sortByDate } from '@/utils/index';
+import { getPosts } from '@/lib/posts';
 
 export default function HomePage({ posts }) {
-  console.log(posts);
-
   return (
     <Layout className=''>
       <h1 className='text-5xl border-b-4 p-5 font-bold'>Latest Posts</h1>
@@ -30,27 +25,9 @@ export default function HomePage({ posts }) {
 }
 
 export async function getStaticProps() {
-  const files = fs.readdirSync(path.join('posts'));
-
-  const posts = files.map((filename) => {
-    const slug = filename.replace('.md', '');
-
-    const markdownWithMeta = fs.readFileSync(
-      path.join('posts', filename),
-      'utf-8'
-    );
-
-    const { data: frontmatter } = matter(markdownWithMeta);
-
-    return {
-      slug,
-      frontmatter,
-    };
-  });
-
   return {
     props: {
-      posts: posts.sort(sortByDate).slice(0, 6),
+      posts: getPosts().slice(0, 6),
     },
   };
 }
