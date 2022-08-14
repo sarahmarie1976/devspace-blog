5fcc895eb81d2b8f579cf31b06c13773c1ed7885
Sarahmarie D Hollenstein <sholle7@gmail.com>
2022-08-14 14:57:45 -0500

    :sparkles: Completed search results component and functionality

--- components/Search.js
+++ components/Search.js
@@ -13,7 +13,6 @@ export default function Search() {
       } else {
         const res = await fetch(`/api/search?q=${searchTerm}`);
         const { results } = await res.json();
-        console.log(results);
         setSearchResults(results);
       }
     };
